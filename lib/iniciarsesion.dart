9c9a56577a82528107752101fc51e15874540d6b
javProg24 <javiertomalag14@gmail.com>
2025-06-18 09:14:38 -0500

    Subiendo fuentes

---
+++ lib/iniciarsesion.dart
@@ -0,0 +1,142 @@
+// login
+import 'package:flutter/material.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: const MyIniciarSesionPage());
+  }
+}
+
+class MyIniciarSesionPage extends StatefulWidget {
+  const MyIniciarSesionPage({super.key});
+  @override
+  State<StatefulWidget> createState() => _MyIniciarSesionPage();
+}
+
+class _MyIniciarSesionPage extends State<MyIniciarSesionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/fondo.png'), // Fondo de pantalla
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                // LOGO O TEXTO SUPERIOR
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: const Text(
+                    'CULTITRAKER',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'PressStart2P',
+                      color: Colors.indigo,
+                      letterSpacing: 2,
+                      shadows: [
+                        Shadow(
+                          color: Colors.black26,
+                          offset: Offset(2, 2),
+                          blurRadius: 4,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                Container(
+                  width: 320,
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.grey.shade300, width: 2),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        blurRadius: 8,
+                        offset: const Offset(2, 4),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text(
+                        'Inicio Sesion',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blueAccent,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      elementosIngreso(),
+                      const SizedBox(height: 20),
+                      botones(),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: const [
+                          Checkbox(value: false, onChanged: null),
+                          Text('Recordar contraseña'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget botones() {
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          width: 300,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text('Iniciar Sesion'),
+          ),
+        ),
+        SizedBox(
+          width: 300,
+          child: ElevatedButton(onPressed: () {}, child: Text('Registrarse')),
+        ),
+      ],
+    );
+  }
+
+  Widget elementosIngreso() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Correo',
+          ),
+        ),
+        const SizedBox(height: 20),
+        TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Contraseña',
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,76 @@
+// pagina de bienvenida
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MainAppCul());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MainAppCul extends StatelessWidget {
+  const MainAppCul({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(home: const MyBievenidaPage());
+  }
+}
+
+class MyBievenidaPage extends StatefulWidget {
+  const MyBievenidaPage({super.key});
+  @override
+  State<StatefulWidget> createState() => _MyBievenidaPage();
+}
+
+class _MyBievenidaPage extends State<MyBievenidaPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/fondo.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: const Text(
+                    'TE DAMOS LA BIENVENIDA A CULTITRAKER',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'PressStart2P',
+                      color: Colors.indigoAccent,
+                      letterSpacing: 2,
+                      shadows: [
+                        Shadow(
+                          color: Colors.black26,
+                          offset: Offset(2, 2),
+                          blurRadius: 4,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: const Text(
+                    'Gestiona tus riegos y cultivos',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.white,
+                      fontFamily: 'Roboto2',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
